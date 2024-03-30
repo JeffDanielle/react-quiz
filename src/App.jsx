5676199a0e42f1987aa99ec9e629ce84d214eb10
JeffDanielle <bayotjeff@gmail.com>
2024-03-30 13:20:27 +0800

    187. Yet Another Hook: useReducer -18min

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './custom.css'
+import DateCounter from './components/DateCounter'
+
+function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  )
+}
+
+export default App
